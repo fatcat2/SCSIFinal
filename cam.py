2a0e83a404ef4bd9d86eb9480a8f6f899cb593e7
XXXXYYYYYY <fishere2@cmc304-01.mathcs.carleton.edu>
2016-07-22 10:17:17 -0500

    Cleansed unused files.
    Deleted files that were either unused or obsolete

--- cam.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2
-
-cam = cv2.VideoCapture(1)
-ret, frame = cam.read()
-for x in 5000:
-    vis = frame.copy()
-    cv2.imshow('camshift', vis)
-cv2.destroyAllWindows()
