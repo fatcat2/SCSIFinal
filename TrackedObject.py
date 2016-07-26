0d8c7670f84106b4f98dc511f6abc0bb64162852
XXXXYYYYYY <fishere2@cmc304-01.mathcs.carleton.edu>
2016-07-26 09:14:09 -0500

    Minor bugfixes/todos

--- TrackedObject.py
+++ TrackedObject.py
@@ -1,23 +1,29 @@
 class TrackedObject:
-	hist = None
-	tracking_window = None
-	centerpoint = (0, 0)
-	def __init__(self, box, center):
-		tracking_window = box
-		centerpoint = center
-	
-	#Getters and Setters
-	def setHist(self, hist):
-		self.hist = hist
-	def setTrackingWindow(self, trackwindow):
-		self.tracking_window = trackingwindow
-	def setCenterPoint(self, cp):
-		self.centerpoint = cp
-	def getHist(self):
-		return hist
-	def getTrackWindow(self):
-		return tracking_window
-	def getCenterPoint(self):
-		return centerpoint
-	def updateObk(self, coords):
-		self.centerpoint = coords
+    hist = None
+    tracking_window = None
+    centerpoint = (0, 0)
+
+    def __init__(self, box, center):
+        self.tracking_window = box
+        self.centerpoint = center
+        # TODO: Set up get hist code to prevent colorshifting over time
+
+        # Getters and Setters
+    def setHist(self, hist):
+        self.hist = hist
+    def setTrackingWindow(self, trackwindow):
+        self.tracking_window = trackingwindow
+    def setCenterPoint(self, cp):
+        self.centerpoint = cp
+    def getHist(self):
+        return hist
+    def getTrackWindow(self):
+        return tracking_window
+    def getCenterPoint(self):
+        return centerpoint
+    def updateObk(self, coords):
+        self.centerpoint = coords
+
+    def update(self, image):
+        pass
+        # TODO: Add code for Camshift
