a9084bf5b27759fe78dbe34a8d47a5951024e1ec
Ryan Chen <ryanjchen@yahoo.com>
2016-07-26 10:00:12 -0500

    Added Camshift update methods to TrackedObject.py

--- TrackedObject.py
+++ TrackedObject.py
@@ -3,6 +3,7 @@ import numpy as np
 class TrackedObject:
     hist = None
     tracking_window = None
+    track_box = None
     centerpoint = (0, 0)
     hsvImage = None
     maskImage = None
@@ -12,17 +13,23 @@ class TrackedObject:
         self.tracking_window = box
         x, y, w, h = box
         self.centerpoint = center
-        self.hsvImage = image
+        setImage(image)
+        self.initHSV()
         # TODO: Set up get hist code to prevent colorshifting over time
     def initHSV(self):
-    	maskImage = cv2.inRange(hsv, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
     	maskROI = maskImage[y:y+h, x:x+w]
     	hsvROI = hsvImage[y:y+h, x:x+w]
     	self.setHist(cv2.calcHist([hsvROI], [0], maskROI, [64], [0, 255]))
+        cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX) #reduces the extremes
+        hist = hist.reshape(-1)	
     	
         # Getters and Setters
     def setHist(self, hist):
         self.hist = hist
+    def setImage(self, hsvImage):
+    	self.maskImage = cv2.inRange(hsv, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
+    	self.hsvImage = hsvImage
+    	self.maskImage()
     def setTrackingWindow(self, trackwindow):
         self.tracking_window = trackingwindow
     def setCenterPoint(self, cp):
@@ -33,9 +40,16 @@ class TrackedObject:
         return tracking_window
     def getCenterPoint(self):
         return centerpoint
+    def getTrackBox(self):
+    	return track_box
     def updateObk(self, coords):
         self.centerpoint = coords
 
     def update(self, image):
+    	setImage(image)
+    	prob = cv2.calcBackProject([hsv], [0], hist, [0, 180], 1)
+    	prob &= maskImage
+        term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 ) #criteria for termination
+        track_box, track_window = cv2.CamShift(prob, track_window, term_crit)
         pass
         # TODO: Add code for Camshift
