ddb68f21e86ce8506150e0a4813120e6db36e7d4
XXXXYYYYYY <fishere2@cmc304-01.mathcs.carleton.edu>
2016-07-27 14:40:59 -0500

    Formatted

--- skeleton.py
+++ skeleton.py
@@ -1,16 +1,23 @@
 import cv2
+
+
 class Skeleton:
-	linkageList = []
-	lineColor = (203, 192, 255)
-	def __init__(self, initLinks=[]):
-		linkageList=initLinks
-		pass
-	def addLink(self, to):
-		linkageList.append(to)
-	def getLink(self, x):
-		return linkageList[x]
-	def renderLink(self, image, index):
-		cv2.line(image, linkageList[index][0], linkageList[index][1], lineColor)
-	def renderAllLinks(self, image):
-		for x in linkageList:
-			cv2.line(image, x[0], x[1], lineColor) 
+    linkageList = []
+    lineColor = (203, 192, 255)
+
+    def __init__(self, initLinks=[]):
+        self.linkageList = initLinks
+        pass
+
+    def addLink(self, to):
+        self.linkageList.append(to)
+
+    def getLink(self, x):
+        return self.linkageList[x]
+
+    def renderLink(self, image, index):
+        cv2.line(image, self.linkageList[index][0], self.linkageList[index][1], self.lineColor)
+
+    def renderAllLinks(self, image):
+        for x in self.linkageList:
+            cv2.line(image, x[0], x[1], self.lineColor)
