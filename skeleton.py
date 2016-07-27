a15bdc908e8020ee2bff552da0d6bbb57ece493e
Ryan Chen <ryanjchen@yahoo.com>
2016-07-27 14:27:02 -0500

    Made skeleton.py ready for first test

--- skeleton.py
+++ skeleton.py
@@ -1,12 +1,16 @@
 import cv2
 class Skeleton:
 	linkageList = []
-	def __init__(self):
+	lineColor = (203, 192, 255)
+	def __init__(self, initLinks=[]):
+		linkageList=initLinks
 		pass
 	def addLink(self, to):
 		linkageList.append(to)
 	def getLink(self, x):
 		return linkageList[x]
 	def renderLink(self, image, index):
-		cv2.line(image, linkageList[index][0], linkageList[index][1], (203, 192, 255))
-		
+		cv2.line(image, linkageList[index][0], linkageList[index][1], lineColor)
+	def renderAllLinks(self, image):
+		for x in linkageList:
+			cv2.line(image, x[0], x[1], lineColor) 
