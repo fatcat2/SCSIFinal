8f9d9e84b9904eee25ae7d05fe82ee20651aaf7d
Ryan Chen <ryanjchen@yahoo.com>
2016-08-01 14:04:46 -0500

    Small changes to code; assembling code together

--- main.py
+++ main.py
@@ -31,9 +31,14 @@ def onmouse(event, x, y, flags, param):
         selection = None
         if x1-x0 > 0 and y1-y0 > 0:
             selection = (x0, y0, x1, y1)  # set current drag rectangle
+def getNextFrame(vidObj):
+    """Takes in the VideoCapture object and reads the next frame, returning one that is half the size 
+    (Comment out that line if you want fullsize)."""
+    ret, frame = vidObj.read()
+    print type(vidObj), type(frame)
+    frame = cv2.resize(frame, (0, 0), fx = 0.5, fy = 0.5)
+    return frame
 
-
-            
 if __name__ == "__main__":
     colorList = [((30, 120, 60), (80, 255, 255)), ((74, 80, 80), (114, 230, 230))]#
     averageColors=mf.getAverageColors(colorList)
@@ -42,43 +47,48 @@ if __name__ == "__main__":
     for i in colorList:
         trackingItems[i] = []
     vidCap = cv2.VideoCapture(0)
-    ret, imgInit = vidCap.read()
-    imgInit = cv2.cvtColor(imgInit, cv2.COLOR_BGR2HSV)
-    contours = mf.evaluateForContours(imgInit, colorList)
-    last = None
-    if contours is not None and len(contours) > 0:
-        centers = mf.trimCenters(mf.getCentersAndBoxes(contours), 50)
-        for c in range(len(centers)):
-            for j in centers[c]:
-                x = to.TrackedObject(mf.intTuple(j[1][0]+j[1][1]), j[0], imgInit, colorList[c])
-                trackingItems[colorList[c]].append(x)
-                if last is not None:
-                    pass # skeleton.addLink(x, last)
-                last = x
-    arm=[None, None, None]
-    print trackingItems
-    for i in trackingItems[colorList[1]]:
-        if arm[0] is None or i.getCenterPoint()[1]<arm[0].getCenterPoint():
-            arm[0]=i
-    for i in trackingItems[colorList[1]]:
-        if i not in arm and \
-                (arm[1] is None or
-                 mf.distance(i.getCenterPoint(), arm[0].getCenterPoint()) <
-                         mf.distance(arm[1].getCenterPoint(), arm[0].getCenterPoint())):
-            arm[1]=i
-    for i in trackingItems[colorList[1]]:
-        if i not in arm and \
-                (arm[1] is None or
-                 mf.distance(i.getCenterPoint(), arm[0].getCenterPoint()) <
-                         mf.distance(arm[1].getCenterPoint(), arm[0].getCenterPoint())):
-            arm[2] = i
-    print arm
-    if None not in [arm[0], arm[1]]:
-        skeleton.addLink(arm[0], arm[1])
-    if None not in [arm[1], arm[2]]:
-        skeleton.addLink(arm[1], arm[2])
+    ret, ouput = vidCap.read()
+    # imgInit = cv2.cvtColor(imgInit, cv2.COLOR_BGR2HSV)
+    # contours = mf.evaluateForContours(imgInit, colorList)
+    # last = None
+    # if contours is not None and len(contours) > 0:
+    #     centers = mf.trimCenters(mf.getCentersAndBoxes(contours), 50)
+    #     for c in range(len(centers)):
+    #         for j in centers[c]:
+    #             x = to.TrackedObject(mf.intTuple(j[1][0]+j[1][1]), j[0], imgInit, colorList[c])
+    #             trackingItems[colorList[c]].append(x)
+    #             if last is not None:
+    #                 pass # skeleton.addLink(x, last)
+    #             last = x
+    # arm=[None, None, None]
+    # print trackingItems
+    # for i in trackingItems[colorList[1]]:
+    #     if arm[0] is None or i.getCenterPoint()[1]<arm[0].getCenterPoint():
+    #         arm[0]=i
+    # for i in trackingItems[colorList[1]]:
+    #     if i not in arm and \
+    #             (arm[1] is None or
+    #              mf.distance(i.getCenterPoint(), arm[0].getCenterPoint()) <
+    #                      mf.distance(arm[1].getCenterPoint(), arm[0].getCenterPoint())):
+    #         arm[1]=i
+    # for i in trackingItems[colorList[1]]:
+    #     if i not in arm and \
+    #             (arm[1] is None or
+    #              mf.distance(i.getCenterPoint(), arm[0].getCenterPoint()) <
+    #                      mf.distance(arm[1].getCenterPoint(), arm[0].getCenterPoint())):
+    #         arm[2] = i
+    # print arm
+    # if None not in [arm[0], arm[1]]:
+    #     skeleton.addLink(arm[0], arm[1])
+    # if None not in [arm[1], arm[2]]:
+    #     skeleton.addLink(arm[1], arm[2])
 
+    cv2.namedWindow('output')
+    cv2.setMouseCallback('output', onmouse)
+
+    #Capture Loop
     while True:
+        frame = getNextFrame(vidCap)
         img1 = cv2.cvtColor(vidCap.read()[1], cv2.COLOR_BGR2HSV)
         origImg=np.copy(img1)
         for i in trackingItems.keys():
