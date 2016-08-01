60e64ea2b463f3dbbf89d14fc5cdf4397cdcd9bb
Ryan Chen <ryanjchen@yahoo.com>
2016-08-01 15:54:28 -0500

    HAVING A HUGE PROBLEM WITH TUPLES MAKE EVERYTHING TUPLES AHHHHHHHHHHHHH

--- main2.py
+++ main2.py
@@ -24,16 +24,19 @@ def onmouse(event, x, y, flags, param):
     global frame
     h, w = frame.shape[:2]           
     if event == cv2.EVENT_LBUTTONDOWN:
+        print "MOUSEDOWN"
         drag_start = (x, y)
         isTracking = False
         isDragging = True
     elif event == cv2.EVENT_LBUTTONUP:  # left mouse button up indicates end of dragging
+        print "MOUSEUP"
         isDragging = False
         drag_start = None
         if selection != None:
             isTracking = True
             # print selection
     elif isDragging and event == cv2.EVENT_MOUSEMOVE:    # if currently dragging and mouse is moving
+        print "MOUSEMOVE"
         xo, yo = drag_start              # first compute upperleft anbd lower right
         x0 = max(0, min(xo, x))
         x1 = min(w, max(xo, x))
@@ -75,35 +78,43 @@ cv2.namedWindow('hist')
 cv2.moveWindow('hist', 700, 100)   # Move to reduce overlap
 
 # start processing frames
-while True:
+for i in range(1000):
+    # print i
     frame = getNextFrame(cam)
     vis = frame.copy()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)    # convert to HSV
     mask = cv2.inRange(hsv, np.array((0., 60., 32.)), np.array((180., 255., 255.)))   # eliminate low and high saturation and value values
 
     if isDragging and selection != None:    # if currently dragging and a good region has been selected
-        x = to.TrackedObject(selection, hsv, mask)
-        trackedObjectList.append(x)
-        show_hist(x.getHist())
-        vis_roi = vis[y0:y1, x0:x1]
-        cv2.bitwise_not(vis_roi, vis_roi)
+        x0, y0, x1, y1 = selection
+        track_window = (x0, y0, x1-x0, y1-y0)
+        hsv_roi = hsv[y0:y1, x0:x1]             # access the currently selected region and make a histogram of its hue 
+        mask_roi = mask[y0:y1, x0:x1]
+        hist = cv2.calcHist( [hsv_roi], [0], mask_roi, [16], [0, 180] )
+        cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX)
+        hist = hist.reshape(-1)
+        show_hist(hist)
         if showHistMask:
             vis[mask == 0] = 0
-        for obj in trackedObjectList:  # If tracking...
-            prob = cv2.calcBackProject([obj.getImage()], [0], obj.getHist(), [0, 180], 1) #calculates the probability of similarity
-            prob &= obj.getMask() #adds the probability of hit rate to the mask
-            #count gets rid after a certain count if it can't settle
-            term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 ) #criteria for termination
-            x, y = cv2.CamShift(prob, y, term_crit) #returns a box for the ellipse to use and box parameters to be used as a search box
-            obj.setTrackBox(x)
-            obj.setTrackingWindow(y)
-            if showBackProj:
-                vis[:] = prob[...,np.newaxis]
-            try:
-                cv2.ellipse(vis, obj.getTrackBox, (0, 0, 255), 2) #draws the red ellipse with a stroke of 2 onto the copy of the frame, and uses the dimensions of the track_box variable
-            except:
-                pass
-                # print track_box
+    if not isDragging and selection != None:
+        x0, y0, x1, y1 = selection
+        x = to.TrackedObject((x0, y0, x1, y1), hsv, mask, hist)
+        trackedObjectList.append(x)
+        show_hist(hist)
+
+    for obj in trackedObjectList:  # If tracking...
+        obj.update(frame)
+        prob = cv2.calcBackProject([obj.getImage()], [0], obj.getHist(), [0, 180], 1) #calculates the probability of similarity
+        prob &= obj.getMask() #adds the probability of hit rate to the mask
+        #count gets rid after a certain count if it can't settle
+        term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 ) #criteria for termination
+        if showBackProj:
+            vis[:] = prob[...,np.newaxis]
+        try:
+            cv2.ellipse(vis, obj.getTrackWindow(), (0, 0, 255), 2) #draws the red ellipse with a stroke of 2 onto the copy of the frame, and uses the dimensions of the track_box variable
+        except:
+            pass
+            # print track_box
     cv2.imshow('camshift', vis)
 
     ch = 0xFF & cv2.waitKey(5)
